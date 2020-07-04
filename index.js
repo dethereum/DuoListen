6c8e2b6a545750c50f751d30fca87889067d5160
Morgan Galvin <morgan@ludwigthepig.com>
2020-07-04 00:29:22 -0600

    the rest of the owl

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+alert("We are running!")
 
+// *What* to Do
 const hideHintSentenceIfExists = () => {
   const testNames = {
     translatePrompt: '[data-test=\'challenge-translate-prompt\']',
@@ -9,14 +11,34 @@ const hideHintSentenceIfExists = () => {
   
   if (hasTranslateTest) {
     const hintSentence = document.querySelector(testNames.hintSentence);
-    hintSentence.style.display = 'none';
+    if (hintSentence !== null) { 
+      hintSentence.innerHTML = 'Translate what you hear. No hints! 🙈)';
+    }
   } 
 }
 
+  // *Who* to observe for changes (the 'submit' button)
+  let targetNode = null;
+  // *When* to fire
+  const observerOptions = {
+    attributes: true,
+  }
+
+  // Invoking
+  const observer = new MutationObserver(hideHintSentenceIfExists);
+  const attachObserverToNode = () => {
+    targetNode = document.querySelector('[data-test="player-next"]');
+    if (targetNode === null) {
+      setTimeout(attachObserverToNode, 1000);
+    } else {
+      observer.observe(targetNode, observerOptions);
+    }
+  };
+  attachObserverToNode();
+
 chrome.runtime.onMessage.addListener(
   (request, sender, sendResponse) => {
-    if (request.message === "clicked_browser_action" ) {
-      console.log('hello');
+    if (request.message === "The button has changed." ) {
       hideHintSentenceIfExists();
     }
   }
